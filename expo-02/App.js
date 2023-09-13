587f8414c2dcdec10f45cf4615a11ce9ad702dfc
Marcio Bueno <marciobueno@gmail.com>
2023-09-13 00:39:26 +0000

    Adicionado React Navigation (projeto expo-02)

---
+++ expo-02/App.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens/HomeScreen";
+import { DetailsScreen } from "./screens/DetailsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ expo-02/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { Button, Text, View } from "react-native";
+
+export const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
